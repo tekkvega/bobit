293e50a5912b83d1dd3da9261c42b3b06f38095c
Mott's Applesauce <josiahvega123@gmail.com>
2022-03-18 22:05:17 -0400

    edited readme and code for json instead of .env for changable prefix

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,19 @@ use serenity::{async_trait,model::{channel::Message, gateway::Ready},prelude::*}
 use std::collections::HashSet;
 use serenity::http::Http;
 use crate::commands::Handler;
+use std::path::Path;
+use std::ffi::OsStr;
+use std::fs;
+use serde::Deserialize;
+use std::error::Error;
+use std::io::BufReader;
+use serde_json::Value;
+
+#[derive(Deserialize, Debug)]
+struct Jstruct {
+    fingerprint: String,
+    location: String,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -28,8 +41,9 @@ impl EventHandler for Handler {
 }
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().expect("Failed to load .env file");
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let filestring = fs::read_to_string("bobit.json");
+    let out: Value = serde_json::from_str(&filestring.unwrap()).unwrap();
+    let token = out["token"].as_str().unwrap();
     let http = Http::new_with_token(&token);
     let (owners, _bot_id) = match http.get_current_application_info().await {
         Ok(info) => {
@@ -40,8 +54,7 @@ async fn main() {
         },
         Err(why) => panic!("Could not access application info: {:?}", why),
     };
-
-    let framework = StandardFramework::new().configure(|c| c.owners(owners).prefix("&")).group(&COMMANDS_GROUP);
+    let framework = StandardFramework::new().configure(|c| c.owners(owners).prefix(out["prefix"].as_str().unwrap())).group(&COMMANDS_GROUP);
     let mut client = Client::builder(&token).framework(framework).event_handler(Handler).await.expect("Err creating client");
 
     if let Err(why) = client.start().await
